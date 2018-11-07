7d0a3c651c036f65111ae9f0aac7e1267f6e519e
CRANKSHAW <s14303742@IT046599.massey.ac.nz>
2018-11-07 16:22:48 +1300

    final version now testing phase

--- assignment4.cu
+++ assignment4.cu
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <fstream>
+
+using namespace std;
 
 #include <cuda.h>
 #include "lenses.h"
@@ -14,7 +17,7 @@
 
 
 // Boundaries in physical units on the lens plane
-const float WL  = 2.0;
+const float WL  = 4.0;
 const float XL1 = -WL;
 const float XL2 =  WL;
 const float YL1 = -WL;
@@ -22,7 +25,7 @@ const float YL2 =  WL;
 
 // Source star parameters. You can adjust these if you like - it is
 // interesting to look at the different lens images that result
-const float rsrc = 0.1;      // radius
+const float rsrc = 0.2;      // radius
 const float ldc  = 0.5;      // limb darkening coefficient
 const float xsrc = 0.0;      // x and y centre on the map
 const float ysrc = 0.0;
@@ -33,8 +36,7 @@ const float ysrc = 0.0;
 double diffclock(clock_t clock1,clock_t clock2)
 {
   double diffticks = clock1 - clock2;
-  double diffms = (diffticks * 1000) / CLOCKS_PER_SEC;
-  return diffms; // Time difference in milliseconds
+  return (diffticks * 1000) / CLOCKS_PER_SEC;
 }
 
 void err_check(cudaError_t err){
@@ -56,12 +58,14 @@ __global__ void ray_shoot(int *maxX, int *maxY, float *lens_scale, float *xlens,
 
 	float xl = XL1 + x * (*lens_scale);
 	float yl = YL1 + y * (*lens_scale); 
-	float xs = XL2 + x * (*lens_scale); 
-	float ys = YL2 + y * (*lens_scale);
+	float xs;
+	float ys;
 
 	float dx, dy, dr;
+
 	xs = xl;
 	ys = yl;
+
 	for(int p = 0; p < (*num_lenses); ++p){
 		dx = xl - xlens[p];
 	    dy = yl - ylens[p];
@@ -83,6 +87,27 @@ __global__ void ray_shoot(int *maxX, int *maxY, float *lens_scale, float *xlens,
 // main routine that executes on the host
 int main(void)
 {
+	//Used for timing the total time
+	clock_t totalTime = clock();
+
+	//Used for timing the cuda kernel
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	float deviceTime = 0.0;
+
+
+	//Used for timing the host cpu
+	clock_t hostTime = clock();
+	double totalHostTime = 0.0;
+
+	//File io
+	ofstream logfile;
+	logfile.open("log.txt");
+	if(!logfile.is_open()){
+		cout << "Cannot open log file for writing!" << endl;
+		exit(1);
+	}
 
 	// Set up lensing system configuration - call example_1, _2, _3 or
 	// _n as you wish. The positions and mass fractions of the lenses
@@ -90,8 +115,8 @@ int main(void)
 	float* xlens;
 	float* ylens;
 	float* eps;
-	const int nlenses = set_example_2(&xlens, &ylens, &eps);
-	std::cout << "# Simulating " << nlenses << " lens system" << std::endl;
+	const int nlenses = set_example_n(10,&xlens, &ylens, &eps);
+	std::cout << "Simulating " << nlenses << " lens system" << std::endl;
 
 
 	// Pixel size in physical units of the lens image. You can try finer
@@ -102,15 +127,14 @@ int main(void)
 	// Size of the lens image
 	const int npixx = static_cast<int>(floor((XL2 - XL1) / lens_scale)) + 1;
 	const int npixy = static_cast<int>(floor((YL2 - YL1) / lens_scale)) + 1;
-	std::cout << "# Building " << npixx << "X" << npixy << " lens image" << std::endl;
+	std::cout << "Building " << npixx << "X" << npixy << " lens image" << std::endl;
 
 	// Put the lens image in this array
   	Array<float, 2> lensim(npixy, npixx);
 
-	clock_t tstart = clock();
-
 	int total_pixels = npixx * npixy;
-	std::cout << "total pixels: " << total_pixels << std::endl;
+
+	std::cout << "Total pixels: " << total_pixels << std::endl;
 	int threadsPerBlock = 1024;
 	std::cout << "total Threads per block: " << threadsPerBlock << std::endl;
 	int numBlocks = (total_pixels + threadsPerBlock - 1) / threadsPerBlock;
@@ -123,9 +147,7 @@ int main(void)
 		for(int x = 0; x < npixx; x++){
 			arr_lensim[(y * npixx) + x] = 0;
 		}
-	}
-
-	std::cout << "host----: " << eps[0] << std::endl;
+	} 
 
 	//--------------------------
 	//cuda part
@@ -160,14 +182,22 @@ int main(void)
 	cudaMemcpy(dev_nlenses, &nlenses, sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_arr_lensim, arr_lensim, sizeof(float)*npixx*npixy, cudaMemcpyHostToDevice);
 
+	totalHostTime = diffclock(clock(), hostTime);
+
 	//=====================================
 	//=====================================
 	//Need to create a new array on both host and device memory and then copy from device the new values and then convert it back to the Array object type so that we can use dump_array();
 	
+	cudaEventRecord(start);
 	ray_shoot<<<numBlocks, threadsPerBlock>>>(dev_npixx, dev_npixy, dev_lens_scale, dev_xlens, dev_ylens, dev_eps, dev_nlenses, dev_arr_lensim);
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+
+	hostTime = clock();
 
 	cudaMemcpy(arr_lensim, dev_arr_lensim, sizeof(float)*npixx*npixy, cudaMemcpyDeviceToHost);
 
+	cudaEventElapsedTime(&deviceTime, start, stop);
 
 	for(int y = 0; y < npixy; y++){
 		for(int x = 0; x < npixx; x++){
@@ -177,14 +207,24 @@ int main(void)
 		}
 	}
 
+	cudaFree(dev_npixx);
+	cudaFree(dev_npixy);
+	cudaFree(dev_lens_scale);
 	cudaFree(dev_xlens); 
 	cudaFree(dev_ylens); 
 	cudaFree(dev_eps);
+	cudaFree(dev_nlenses);
 	cudaFree(dev_arr_lensim);
 
-	clock_t tend = clock();
-	double tms = diffclock(tend, tstart);
-	std::cout << "# Time elapsed: " << tms << " ms " << std::endl;
+	totalHostTime += diffclock(clock(), hostTime);
+	std::cout << "Total Host Time = " << totalHostTime << " ms " << std::endl;
+
+	std::cout << "Total Device Time = " << deviceTime << " ms " << std::endl;
+	double diff_totaltime = diffclock(clock(), totalTime);
+	std::cout << "Total Program Time = " << diff_totaltime << " ms " << std::endl;
+
+	logfile << "number of lenses, lens scale, x pixels, y pixels, Total host time,Total device time, total program time\n";
+	logfile << nlenses << "," << lens_scale << "," << npixx << "," << npixy << "," << totalHostTime << "," << deviceTime << "," << diff_totaltime <<"\n";
 
 	// Write the lens image to a FITS formatted file. You can view this
 	// image file using ds9
@@ -193,4 +233,6 @@ int main(void)
 	delete[] xlens;
 	delete[] ylens;
 	delete[] eps;
+
+	free(arr_lensim);
 }
